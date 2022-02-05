0601405c051bf567fc9dab2a15fa59bc6bd204a3
it62070180 <56427172+it62070180@users.noreply.github.com>
2022-02-05 14:44:13 +0700

    feat: router
    feat: MainApp icon
    type: Add bootstrap-icons
    description icon

--- sdte-tnku/src/App.vue
+++ sdte-tnku/src/App.vue
@@ -1,25 +1,30 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-info">
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link class="navbar-brand" aria-current="page" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="navbar-brand" to="Result">Details</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="navbar-brand" to="Detail">Result</router-link>
-        </li>
-      </ul>
+<div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-info">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+           
+          <li class="nav-item">
+           
+            <router-link class="navbar-brand on-active" aria-current="page" to="/">
+            <img src="./assets/compass.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"> 
+            Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="navbar-brand" to="Result">Details</router-link>
+          </li>
+          <!-- เอาออกที่หลัง -->
+          <li class="nav-item">
+            <router-link class="navbar-brand" to="Detail">Result</router-link>
+          </li>
+          <!-- เอาออกที่หลัง -->
+        </ul>
     </div>
-  </div>
-</nav>
+  </nav>
+   <router-view/>
+</div>
 
- <router-view></router-view>
-
- 
 </template>
 
 <script>
