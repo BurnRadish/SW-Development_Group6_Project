ccda521588b47e6c9ca797fd777afc24ea307e7d
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2022-02-12 15:13:12 +0700

    feat : Review Comment : [add] : เพิ่ม component ReviewBox

---
+++ sdte-tnku/src/components/ReviewBox.vue
@@ -0,0 +1,56 @@
+<template>
+<section>
+    <div class="box">
+    <div class="row">
+      <div class="col-3"  v-for="n in 4" :key="n">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex flex-start align-items-center">
+              <img
+                class="rounded-circle shadow-1-strong me-3"
+                src="https://i.imgur.com/AD3MbBi.jpeg"
+                alt="avatar"
+                width="60"
+                height="60"
+              />
+              <div>
+                <h6 class="fw-bold text-primary mb-1">{{this.name}}</h6>
+                <p class="text-muted small mb-0">
+                  Score : {{this.score}}/5
+                </p>
+              </div>
+            </div>
+
+            <p class="mt-3 mb-4 pb-2">
+              {{this.msg}}
+            </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  
+  
+</template>
+
+<script>
+export default {
+    name: 'ReviewBox',
+    props: {
+        name: String,
+        msg: String,
+        score: Number,
+    }
+}
+</script>
+
+<style scoped>
+.box {
+    width: 100%;
+    height: 200px;
+    padding: 10px;
+}
+
+</style>
