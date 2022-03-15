f32bcc1d827cd5d86484c34af6963aec4f6e7557
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2022-03-15 22:50:49 +0700

    System : setup backend
    - สร้าง backend สำหรับ ดึง comment ใน mongoDB

---
+++ sdte-tnku/backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const cors = require('cors')
+const path = require("path")
+const app = express();
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const { MongoClient } = require("mongodb");
+const uri = process.env.mongoDB;
+
+const client = new MongoClient(uri);
+
+
+app.get("/getcomments/:id", async(req,res,next)=>{
+  
+  const id = String(req.params.id);
+  let result;
+  await client.connect()
+  const session = client.startSession();
+  try{
+      result = await client.db("sdte").collection("comment").find({ place_id: id }).toArray();
+      res.status(200).send(result)
+  }catch(error){
+      console.log(error)
+  }
+  console.log(result);
+})
+
+app.listen(9000, () => {
+  console.log('Application is running on port 9000')
+})
